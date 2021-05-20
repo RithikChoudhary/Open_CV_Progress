5806291a91ed1af4aa7eddc99d2159fd63a1d947
Rithik Choudhary <40898779+burnt776@users.noreply.github.com>
2021-05-20 21:34:24 +0530

    Blue Mr.india
    Be the Mr.India only in Blue Color

--- invisible clow.py
+++ invisible clow.py
@@ -1,8 +1,9 @@
+# Be the Mr.India only in Blue Color
+
+
 import cv2
 import numpy as np
 
-def nothing():
-    pass
 cap = cv2.VideoCapture(0)
 while True:
     cv2.waitKey(1000)
@@ -19,11 +20,6 @@ def invisible():
         mask = cv2.inRange(hsv,lower_hsv,upper_hsv)
         
         frame[np.where(mask==255)]=init_frame[np.where(mask==255)]
-       
-        cv2.putText(frame,"Red",(20,50),cv2.FONT_HERSHEY_COMPLEX,.5,(0,0,255),2,cv2.LINE_AA)
-        cv2.putText(frame,"Green",(80,50),cv2.FONT_HERSHEY_COMPLEX,.5,(0,255,0),2,cv2.LINE_AA)
-        cv2.putText(frame,"Yellow",(150,50),cv2.FONT_HERSHEY_COMPLEX,.5,(25,255,255),2,cv2.LINE_AA)
-        cv2.putText(frame,"Blue",(240,50),cv2.FONT_HERSHEY_COMPLEX,.5,(255,255,105),2,cv2.LINE_AA)
         
         cv2.imshow('frame',frame)
         cv2.imshow('mask',mask)
