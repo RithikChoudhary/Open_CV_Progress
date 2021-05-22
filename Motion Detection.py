a5767b8841d2917073b0d9926a11864f8137c8f7
Rithik Choudhary <40898779+burnt776@users.noreply.github.com>
2021-05-22 18:43:12 +0530

    Update Motion Detection.py

--- Motion Detection.py
+++ Motion Detection.py
@@ -6,9 +6,9 @@ def Motion_Detection():
     
     while True:
     
-        _,frame1 = cap.read()           #getting first frame
-        _,frame2 = cap.read()           #getting second frame
-        sub = cv2.absdiff(frame1,frame2)  # Taking the diffrence of the frames so that camera only detect the diffrence of motion in both frames
+        _,frame1 = cap.read()            #getting first frame
+        _,frame2 = cap.read()            #getting second frame
+        sub = cv2.absdiff(frame1,frame2) #Taking the diffrence of the frames so that camera only detect the diffrence of motion in both frames
                                                                                     
         gray = cv2.cvtColor(sub,cv2.COLOR_BGR2GRAY)
         edge = cv2.Canny(gray,100,255)                                      # Detecting the edges for drawing contours
